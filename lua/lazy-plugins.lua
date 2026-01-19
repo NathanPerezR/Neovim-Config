3513567cf9e6ea3332746d28bca70b943ce2c316
Nathan Perez <me@nathanperez.dev>
2026-01-19 00:37:19 -0600

    telescope fix

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -24,6 +24,7 @@ require('lazy').setup({
   require 'plugins.autopairs',
   require 'plugins.neo-tree',
   require 'plugins.vimtex',
+  require 'plugins.roslyn'
 --   require 'plugins.mini',
 }, {
   ui = {
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -111,7 +111,7 @@ return {
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -146,10 +146,14 @@ return {
         clangd = {},
         ts_ls = {},
         cssls = {},
-        omnisharp_mono = {}
       }
 
-      require('mason').setup()
+      require('mason').setup({
+        registries = {
+            "github:mason-org/mason-registry",
+            "github:Crashdummyy/mason-registry",
+        },
+      })
 
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
